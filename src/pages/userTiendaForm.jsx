f2f15f75475c4ebe877a97ef1a1cce2312c3c0af
Marmax73 <almafuertebravo@hotmail.com>
2025-07-26 21:11:52 -0300

    ajuste de margin en camaraCaptura

--- src/pages/userTiendaForm.jsx
+++ src/pages/userTiendaForm.jsx
@@ -6,7 +6,7 @@ const userTiendaForm = () => {
         <div className="p-4 display flex flex-col items-center justify-center gap-4 text-center">
             <h1>Bienvenida estas por ingresar como Vendedora</h1>
             <p>Aquí podrás encontrar una variedad de productos y servicios.</p>
-            <div className="flex w-5 h-4 justify-items-center p-3 mb-2 display-block">
+            <div className="flex w-5 h-4 justify-items-center p-3 mb-2 display-block m-auto">
                 <h4>Escanéa tus productos</h4>
                 <CamaraCaptura />
             </div>
